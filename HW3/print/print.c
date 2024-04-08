1d75d950e60d0b6fb64423138a6a4d53c97ed065
Khalidjaris <80885249+Khalidjaris@users.noreply.github.com>
2024-04-07 20:21:27 -0500

    HW3

--- HW3/print/print.c
+++ HW3/print/print.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
-#define BUT_PIN 16
 
 int main() {
     stdio_init_all();
+
+    printf("Start!\n");
+ 
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
     
     // LED
-    #ifndef PICO_DEFAULT_LED_PIN
-    #warning blink example requires a board with a regular LED
-    #else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    const uint LED_PIN = 15;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
 
     // Button
+    const uint BUT_PIN = 16;
     gpio_init(BUT_PIN);
     gpio_set_dir(BUT_PIN, GPIO_IN);
 
@@ -24,27 +26,24 @@ int main() {
     adc_gpio_init(26); // set ADC0 pin to be adc input instead of GPIO
     adc_select_input(0); // select to read from ADC0
 
-    while (!stdio_usb_connected()) {
-        sleep_ms(100);
-    }
     printf("Start!\n");
- 
+
     while (1) {
+        gpio_put(LED_PIN, 1);
+        while (gpio_get(BUT_PIN) == 0) {}
+        gpio_put(LED_PIN, 0);
 
-        while (true) {
-            
+        int samples;
+        printf("Enter a number of analog samples to take: ");
+        scanf("%d", &samples);
+        printf("\r\nSamples: %d\r\n", samples);
+        sleep_ms(2000);
+
+        for(int i = 0; i < samples; i++) {
+            uint16_t result = adc_read();
+            printf("Sample %d: %d\r\n", i+1, result);
+            sleep_ms(10);
         }
-
-        while (true) {
-            gpio_put(LED_PIN, 0);
-            int samples = 0;
-            scanf("Enter a number of analog samples to take: %d\r\n", samples);
-            for(int i = 0; i < samples; i++) {
-                uint16_t result = adc_read();
-                printf("Sample %d: %d\r\n", i+1, result);
-                sleep_ms(1);
-
-            }
-        }
+        sleep_ms(50);
     }
 }
