84516eb610f33bc3c6650fe8913f675cfd143f8d
Khalidjaris <80885249+Khalidjaris@users.noreply.github.com>
2024-06-01 19:38:49 -0500

    HW13 – IMU

--- HW13 – IMU/build/CMakeFiles/main.dir/Users/Omniajam/Desktop/me433/Pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb_descriptors.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/main.dir/Users/Omniajam/Desktop/me433/Pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb_descriptors.c.obj: \
- /Users/Omniajam/Desktop/me433/Pico/pico-sdk/src/rp2_common/pico_stdio_usb/stdio_usb_descriptors.c
---
+++ HW13 – IMU/build/CMakeFiles/mouse.dir/Users/Omniajam/Desktop/me433/Pico/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c.obj.d
@@ -0,0 +1,6 @@
+CMakeFiles/mouse.dir/Users/Omniajam/Desktop/me433/Pico/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c.obj: \
+ /Users/Omniajam/Desktop/me433/Pico/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c \
+ /Users/Omniajam/Desktop/me433/Pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ /Users/Omniajam/Desktop/me433/Pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
+ /Users/Omniajam/Documents/GitHub/me433/HW13\ – IMU/tusb_config.h \
+ /Users/Omniajam/Desktop/me433/Pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
